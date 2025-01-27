e1cf123f90b5441edd7abeff5a7cea91cfcf7008
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-01-27 10:35:33 +0300

    feat(back): implement authentication and admin resources with Keycloak integration

---
+++ back/src/main/java/org/authmanager/AuthResource.java
@@ -0,0 +1,47 @@
+package org.authmanager;
+
+import jakarta.annotation.security.PermitAll;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.AccessTokenResponse;
+
+@Path("/auth")
+public class AuthResource {
+    @ConfigProperty(name = "keycloak.url")
+    private String SERVER_URL;
+    @ConfigProperty(name = "keycloak.realm")
+    private String REALM;
+    @ConfigProperty(name = "keycloak.client-id")
+    private String CLIENT_ID;
+    @ConfigProperty(name = "keycloak.client-secret")
+    private String CLIENT_SECRET;
+
+    @POST
+    @Path("/login")
+    @PermitAll
+    public Response login(Credentials credentials) {
+        try {
+            Keycloak keycloak = KeycloakBuilder.builder()
+                    .serverUrl(SERVER_URL)
+                    .realm(REALM)
+                    .clientId(CLIENT_ID)
+                    .clientSecret(CLIENT_SECRET)
+                    .grantType("password")
+                    .username(credentials.username)
+                    .password(credentials.password)
+                    .build();
+
+            AccessTokenResponse tokenResponse = keycloak.tokenManager().grantToken();
+            return Response.ok(tokenResponse).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("Échec de l'authentification : " + e.getMessage())
+                    .build();
+        }
+    }
+}
---
+++ back/src/main/java/org/authmanager/Credentials.java
@@ -0,0 +1,13 @@
+package org.authmanager;
+
+public class Credentials {
+    public String username;
+    public String password;
+
+    public Credentials() {}
+
+    public Credentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
