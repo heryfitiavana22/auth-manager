c326c6d8341ce42e76b68eb38ada58211fe490af
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-01-27 11:01:17 +0300

    feat(back): add user info endpoint to AdminResource for token verification

--- back/src/main/java/org/authmanager/AdminResource.java
+++ back/src/main/java/org/authmanager/AdminResource.java
@@ -1,10 +1,15 @@
 package org.authmanager;
 
+import org.keycloak.TokenVerifier;
+import org.keycloak.representations.AccessToken;
+
 import io.quarkus.security.Authenticated;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/api/admin")
 @Authenticated
@@ -14,5 +19,26 @@ public class AdminResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String admin() {
         return "granted";
+    }
+
+    @GET
+    @Path("/user")
+    public Response getUserInfo(@HeaderParam("Authorization") String authHeader) {
+        try {
+            System.out.println(authHeader);
+            String token = authHeader.replace("Bearer ", "");
+
+            // Vérification et décodage du token
+            TokenVerifier<AccessToken> tokenVerifier = TokenVerifier.create(token, AccessToken.class)
+                    .parse();
+
+            AccessToken accessToken = tokenVerifier.getToken();
+
+            return Response.ok(accessToken).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("Impossible de récupérer les informations de l'utilisateur : " + e.getMessage())
+                    .build();
+        }
     }
 }
--- back/src/main/java/org/authmanager/AuthResource.java
+++ back/src/main/java/org/authmanager/AuthResource.java
@@ -44,4 +44,5 @@ public class AuthResource {
                     .build();
         }
     }
+
 }
